65e4ef5366af77a2d6e213d16a29cd85d14b783c
zlm1008 <zlm1008@wildcats.unh.edu>
2023-04-26 02:15:05 -0400

    dormrate2a

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(awsconfig);
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
